fbd6a4becd3e1089e6f2c13f72d5f93d1deebc3b
Nigam Shah <nshah@zynga.com>
2013-08-23 15:24:58 -0400

    First commit of unity project, some art, mono project

---
+++ unity/Assets/Game/Scripts/Test/Tester.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tester : MonoBehaviour {
+	
+	void Awake() {
+		print("Tester.Awake()");
+	}
+	
+	
+	// Use this for initialization
+	void Start () {
+		print("Tester.Start()");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
