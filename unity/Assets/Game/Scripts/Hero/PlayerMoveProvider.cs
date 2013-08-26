4a6947dcc974b8e7cfabe4177292f419a2006f56
Nigam Shah <nshah@zynga.com>
2013-08-26 19:32:04 -0400

    hero on screen and moving horizontally

---
+++ unity/Assets/Game/Scripts/Hero/PlayerMoveProvider.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveProvider : MonoBehaviour {
+
+	// public vars -- can be changed in Inspector
+
+	// expressed in pixels per second
+	public float InitialSpeed = 900f;
+
+	private float m_currentSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+		ResetSpeed();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float hAxisValue = Input.GetAxis("Horizontal");
+		//print("hAxisValue = " + hAxisValue);
+		
+		if (hAxisValue != 0) {
+			Move(hAxisValue);
+		}
+	}
+
+	void Move(float axisValue) {
+		float translation = axisValue * m_currentSpeed * Time.deltaTime;
+		transform.Translate(translation, 0, 0);
+	}
+
+	void ResetSpeed() {
+		m_currentSpeed = InitialSpeed;
+	}
+	void SetSpeed(float speed) {
+		m_currentSpeed = speed;
+	}
+
+	void DisableMovement() {
+		this.enabled = false;
+	}
+	void EnableMovement() {
+		this.enabled = true;
+	}
+}
--- unity/Assets/Game/Scripts/Test/Tester.cs
+++ unity/Assets/Game/Scripts/Test/Tester.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 
 public class Tester : MonoBehaviour {
-	
+
+	private GameObject m_hero;
+
 	void Awake() {
 		print("Tester.Awake()");
 	}
@@ -11,11 +13,27 @@ public class Tester : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		print("Tester.Start()");
+
+		m_hero = GameObject.Find("Hero");
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		float fireAxisValue = Input.GetAxis("Fire1");
+		if (fireAxisValue != 0) {
+			print("Fire1 !!! " + fireAxisValue);
+		}
+
+		fireAxisValue = Input.GetAxis("Fire2");
+		if (fireAxisValue != 0) {
+			print("Fire2 ??? " + fireAxisValue);
+		}
+
+		fireAxisValue = Input.GetAxis("Fire3");
+		if (fireAxisValue != 0) {
+			print("Fire3 +++ " + fireAxisValue);
+		}
 	}
 }
