6dbe1e704559c4f0fa16e4cb072cd858d2447354
Nigam Shah <nshah@zynga.com>
2013-08-29 19:07:47 -0400

    Basic HUD created using GUIText

---
+++ unity/Assets/Game/Scripts/HUD/HUD.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+
+	private GUIText m_scoreLabel;
+	private GUIText m_livesLabel;
+	private GUIText m_calloutLabel;
+
+	// Use this for initialization
+	void Start () {
+		m_scoreLabel = GameObject.Find("Score").guiText;
+		m_livesLabel = GameObject.Find("Lives").guiText;
+		m_calloutLabel = GameObject.Find("Callout").guiText;
+
+		SetScoreText(0);
+		SetLivesText(3);
+
+		DoCallout("INCOMING WAVE", 3);
+	}
+
+	private void SetScoreText(int score) {
+		m_scoreLabel.text = "SCORE : " + score;
+	}
+
+	private void SetLivesText(int lives) {
+		m_livesLabel.text = "LIVES : " + lives;
+	}
+	private void SetCalloutText(string callout) {
+		m_calloutLabel.text = callout;
+	}
+	private void ClearCalloutText() {
+		SetCalloutText("");
+	}
+
+	private void DoCallout(string callout, float time = 1.0f) {
+		SetCalloutText(callout);
+		Invoke("ClearCalloutText", time);
+	}
+}
